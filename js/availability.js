5cd2c92e8e32b7298c48723273908dbb8c64b29c
uwaizz07 <uwaizz07@gmail.com>
2026-08-25 11:39:07 +0530

    added

--- js/availability.js
+++ js/availability.js
@@ -135,9 +135,7 @@ function generateTimeSlots(startTime, endTime, durationMinutes) {
 }
 
 export function getMinBookingDate() {
-  const tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  return tomorrow.toISOString().split('T')[0];
+  return new Date().toISOString().split('T')[0];
 }
 
 export function getMaxBookingDate() {
